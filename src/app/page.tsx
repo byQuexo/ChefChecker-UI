e232d3f5c8ed3c74b1f712b83bd0afd60ad659df
Manita <manitatamang1112@gmail.com>
2024-10-22 18:08:31 +0100

    created login and register as two seperate hild componets and LoginRegister as parent component for it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import LoginRegister from "./components/LoginRegister";
+import LoginRegister from "./utils/components/LoginRegister";
 
 export default function Page(){
     return(

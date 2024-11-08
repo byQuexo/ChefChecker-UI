af9764b59c23e73e6f9fd67ac0bb1fb9391b2c4d
Hannah-Ann <72127228+hannahannx@users.noreply.github.com>
2024-11-08 14:30:33 +0000

    upddteing main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,26 @@
 import SearchBar from "./utils/components/searchBar";
 import FilterBar from "./utils/components/filterBar";
 import LoginRegister from "./utils/components/LoginRegister";
+import {useState} from "react";
+//import { Recipe } from "../stores/types";
+
 
 export default function Home() {
+  const [selecredFilter,setSelectedFilter] = useState(null)
+
+  //API call to get all the data
+ // const filteredSearch = Recipe.filter((recipe: { title: string; }) => {
+ //   recipe.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+//} )
+
+  //Search filter 
+  //const [query,setQuery] = useState("")
+ // const handleSearchChange = event => {
+  //  setQuery(event.target.value)
+ // }
+
+  
+
   return (
     <>
     <div>
@@ -14,7 +32,8 @@ export default function Home() {
       {/**/}
       </div>
       <FilterBar/>
-      {/*comment <LoginRegister/> */}
+      <br></br>
+       <LoginRegister/> 
     </div>
     </>
   )

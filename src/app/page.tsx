e4c66a1eb5118666d9788b68214125b02b581d4a
Hannah-Ann <72127228+hannahannx@users.noreply.github.com>
2024-11-12 10:38:41 +0000

    ui for search and filter bar complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,39 @@
 import SearchBar from "./utils/components/searchBar";
 import FilterBar from "./utils/components/filterBar";
 import LoginRegister from "./utils/components/LoginRegister";
-import {useState} from "react";
+import { useState } from "react";
 //import { Recipe } from "../stores/types";
 
 
 export default function Home() {
-  const [selecredFilter,setSelectedFilter] = useState(null)
+  const [selecredFilter, setSelectedFilter] = useState(null)
 
   //API call to get all the data
- // const filteredSearch = Recipe.filter((recipe: { title: string; }) => {
- //   recipe.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
-//} )
+  // const filteredSearch = Recipe.filter((recipe: { title: string; }) => {
+  //   recipe.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  //} )
 
   //Search filter 
   //const [query,setQuery] = useState("")
- // const handleSearchChange = event => {
+  // const handleSearchChange = event => {
   //  setQuery(event.target.value)
- // }
+  // }
 
-  
+
 
   return (
     <>
-    <div>
-      {/* Top navigation Bar */}
-      <div className="flex justify-center">
-      <img src={require("./utils/assets/Logo.png")} className="text-left" alt="ChefCheckerLogo" height="200" />
-      <SearchBar/>
-      {/**/}
-      </div>
-      <FilterBar/>
-      <br></br>
-       <LoginRegister/> 
-    </div>
-    </>
-  )
+        {/* Top navigation Bar */}
+        <div>
+          {/* Logo */}
+          <div className="bg-[url('/assets/Logo.png')]" > <div/>
+            <SearchBar />
+            {/**/}
+          </div>
+          <FilterBar />
+          <br></br>
+          <LoginRegister />
+        </div>
+      </>
+      )
 }

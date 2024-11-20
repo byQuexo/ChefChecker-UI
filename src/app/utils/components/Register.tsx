716e5eb6a4ba1fb60102a90ea2ee0aaf4311ffc2
Manita Tamang <91623191+5tamam00@users.noreply.github.com>
2024-11-20 20:21:39 +0000

    padding in textbox

--- src/app/utils/components/Register.tsx
+++ src/app/utils/components/Register.tsx
@@ -29,21 +29,21 @@ export default function Register ({
       <label htmlFor="username" className={`mt-2 text-sm 
          ${darkMode ? 'text-white' : 'text-gray-600'}`}>Username</label>
       <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)} required
-      className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      className="block w-full rounded-md border-0 py-2 pl-3 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
 
       <div className="mb-4">
       <label htmlFor="email" className={`mt-2 text-sm 
         ${darkMode ? 'text-white' : 'text-gray-600'}`}>Email address</label>
       <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required
-      className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      className="block w-full rounded-md border-0 py-2 pl-3 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
 
       <div className="mb-4">
       <label htmlFor="password" className={`mt-2 text-sm 
         ${darkMode ? 'text-white' : 'text-gray-600'}`}>Password</label>
       <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required
-      className="block w-full rounded-md border-0 py-1.5 text-black- shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+      className="block w-full rounded-md border-0 py-2 pl-3 text-black- shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
 
       <button type="submit"

e140d34fd0fc9301d02529a96dc9a85f305593fa
Manita Tamang <91623191+5tamam00@users.noreply.github.com>
2024-11-20 20:19:31 +0000

    added padding in textbox

--- src/app/utils/components/Login.tsx
+++ src/app/utils/components/Login.tsx
@@ -27,13 +27,13 @@ export default function Login({
       <div className="mb-4">
           <label htmlFor="email" className={`${darkMode ? 'bg-gray-800' : 'bg-white'}`}>Email address</label>
           <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required
-          className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          className="block w-full rounded-md border-0 py-2 pl-3 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
 
       <div className="mb-4">
           <label htmlFor="password" className={`${darkMode ? 'bg-gray-800' : 'bg-white'}`}>Password</label>
           <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} required
-          className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          className="block w-full rounded-md border-0 py-2 pl-3 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
       </div>
 
       <button type="submit"
